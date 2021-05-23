58606b2b2a56a1a75798f4aaf24c466c20477377
KADILE <munsala-cedric.kadile@epitech.eu>
2021-05-23 08:39:36 +0200

    ..

--- project/src/components/HelloWorld.vue
+++ project/src/components/HelloWorld.vue
@@ -1,15 +1,19 @@
 <template>
   <div class="hello">
-
-    <div class="button">
-      <md-button v-for="item in post" :key="item.data.id" onclick="created">Default</md-button>
+<!--    <div :key="index" v-for="index in posts">Post-->
+<!--    <h1>{{ posts.title}}</h1>-->
+<!--    </div>-->
+    <div :key="index" v-for="index in posts">Post
+      <h1>{{ posts.id}}</h1>
     </div>
-
   </div>
 </template>
 
 <script>
-const axios = require("axios");
+import axios from 'axios'
+// const axios = require("axios");
+// const { remote, ipcRenderer } = require("electron")
+// const { Menu } = remote
 
 
 export default {
@@ -20,19 +24,56 @@ export default {
   },
   data() {
     return {
-      post: []
+      posts: []
     }
   },
   created() {
     // this.initMenu();
     axios.get("http://localhost:8888/wordpress/wp-json/wp/v2/posts/")
         .then(response => {
-        console.log(response)
+          console.log(response)
+          this.posts = response.data.data
         })
         .catch(error => {
           console.log(error)
         })
   },
+  // methods: {
+  //   initMenu() {
+  //     const menu = Menu.buildFromTemplate([
+  //       {
+  //         label: "File",
+  //         submenu: [
+  //           { label: "New Window" },
+  //           {
+  //             label: "Settings",
+  //             accelerator: "CmdOrCtrl+,",
+  //             click: () => {
+  //               ipcRenderer.send("toggle-settings");
+  //             }
+  //           },
+  //           { type: "separator" },
+  //           {
+  //             label: "Quit",
+  //             accelerator: "CmdOrCtrl+Q"
+  //           }
+  //         ]
+  //       },
+  //       {
+  //         label: "Edit",
+  //         submenu: [
+  //           { label: "Menu Item 1" },
+  //           { label: "Menu Item 2" },
+  //           { label: "Menu Item 3" }
+  //         ]
+  //       }
+  //     ]);
+  //     Menu.setApplicationMenu(menu);
+  //   },
+  //   openImage(image) {
+  //     ipcRenderer.send("toggle-image", image)
+  //   }
+  // }
   // created: function () {
   //   this.$http.get('wp/v2/reddit').then(response => {
   //     for (let reddit in response.data){
@@ -53,17 +94,47 @@ export default {
 h3 {
   margin: 40px 0 0;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.list-group {
+  display: -ms-flexbox;
+  display: flex;
+  -ms-flex-direction: column;
+  flex-direction: column;
+  padding-left: 0;
+  margin-bottom: 0;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.list-group-item {
+  position: relative;
+  display: block;
+  padding: 0.75rem 1.25rem;
+  margin-bottom: -1px;
+  background-color: #fff;
+  border: 1px solid rgba(0, 0, 0, 0.125);
 }
-button {
-  background: black;
+.list-group-flush .list-group-item {
+  border-right: 0;
+  border-left: 0;
+  border-radius: 0;
 }
+.list-group-flush:first-child .list-group-item:first-child {
+  border-top: 0;
+}
+.flex-container {
+  display: flex;
+  align-items: center;
+}
+.thumbnail {
+  width: 60px;
+  height: 60px;
+  border-radius: 30px;
+  margin-right: 16px;
+}
+.list-group-item {
+  cursor: pointer
+}
+.list-group-item:hover {
+  background-color: #eee;
+}
+
 /*a {*/
 /*  color: #42b983;*/
 /*}*/
